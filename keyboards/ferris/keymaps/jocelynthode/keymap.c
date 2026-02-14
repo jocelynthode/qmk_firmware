dbdb6af0f069eb7d5425cc08ad539bd0d919f3e6
Jocelyn Thode <jocelyn@thode.email>
2026-02-14 01:37:13 +0100

    Use ergol keymap-extras

--- keyboards/ferris/keymaps/jocelynthode/keymap.c
+++ keyboards/ferris/keymaps/jocelynthode/keymap.c
@@ -1,22 +1,23 @@
 #include QMK_KEYBOARD_H
 #include "enums.h"
+#include "keymap_ergol.h"
 
 // homerow mod-taps (aliases for readability + re-use in combo definitions)
-#define HM_A    LALT_T(KC_A)
-#define HM_S    LGUI_T(KC_S)
-#define HM_D    LSFT_T(KC_D)
-#define HM_F    LCTL_T(KC_F)
-#define HM_J    RCTL_T(KC_J)
-#define HM_K    RSFT_T(KC_K)
-#define HM_L    RGUI_T(KC_L)
-#define HM_SCLN LALT_T(KC_SCLN)
+#define HM_A LALT_T(EL_A)
+#define HM_S LGUI_T(EL_S)
+#define HM_E LSFT_T(EL_E)
+#define HM_N LCTL_T(EL_N)
+#define HM_R RCTL_T(EL_R)
+#define HM_T RSFT_T(EL_T)
+#define HM_I RGUI_T(EL_I)
+#define HM_U LALT_T(EL_U)
 
 
-const uint16_t PROGMEM jk_combo[] = {HM_J, HM_K, COMBO_END};
-const uint16_t PROGMEM cv_combo[] = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM rt_combo[] = {HM_R, HM_T, COMBO_END};
+const uint16_t PROGMEM cv_combo[] = {EL_C, EL_V, COMBO_END};
 /*CW_TOGG*/
 combo_t key_combos[] = {
-    COMBO(jk_combo, KC_ESC),
+    COMBO(rt_combo, KC_ESC),
     COMBO(cv_combo, KC_CAPS),
 };
 
@@ -29,10 +30,10 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT(
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_BASE] = LAYOUT(
-        KC_Q, KC_W, KC_E, KC_R, KC_T,       KC_Y, KC_U, KC_I,    KC_O,   KC_P,
-        HM_A, HM_S, HM_D, HM_F, KC_G,       KC_H, HM_J, HM_K,    HM_L,   HM_SCLN,
-        KC_Z, KC_X, KC_C, KC_V, KC_B,       KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
+    [_BASE] = LAYOUT(
+        EL_Q, EL_C, EL_O, EL_P, EL_W,       EL_J, EL_M, EL_D,   EL_1DK, EL_Y,
+        HM_A, HM_S, HM_E, HM_N, EL_F,       EL_L, HM_R, HM_T,   HM_I,   HM_U,
+        EL_Z, EL_X, EL_MINS, EL_V, EL_B,    EL_DOT, EL_H, EL_G, EL_COMM, EL_K,
         LT(_NUM,KC_TAB), KC_SPC,  LT(_SYMBOL,KC_ENT), LT(_NAV,KC_BSPC)
     ),
 
@@ -51,15 +52,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 	[_MEDIA] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_MPRV,                  KC_MNXT,           XXXXXXX,             XXXXXXX, XXXXXXX, KC_PRINT_SCREEN,
+        QK_BOOTLOADER, QK_REBOOT, XXXXXXX, XXXXXXX,  KC_MPRV,                  KC_MNXT,           XXXXXXX,             XXXXXXX, XXXXXXX, KC_PRINT_SCREEN,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BRIGHTNESS_UP,         KC_AUDIO_VOL_UP,   KC_MEDIA_PLAY_PAUSE, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BRIGHTNESS_DOWN,       KC_AUDIO_VOL_DOWN, KC_AUDIO_MUTE,       XXXXXXX, XXXXXXX, XXXXXXX,
                                                 _______, _______,   _______,_______
     ),
 	[_SYMBOL] = LAYOUT(
-        KC_CIRC, RALT(KC_W), RALT(KC_E), KC_DLR,  KC_PERC,       RALT(KC_Y), KC_AMPR,    KC_ASTR,       KC_QUOT,      KC_GRV,
-        KC_LCBR, RALT(KC_S), RALT(KC_D), KC_RCBR, KC_EQUAL,      KC_BSLS,    KC_PLUS,    RALT(KC_K),    RALT(KC_L),   KC_DQT,
-        KC_TILD, KC_LBRC,    KC_RBRC,    KC_UNDS, RALT(KC_B),    KC_PIPE,    RALT(KC_M), RALT(KC_COMM), RALT(KC_DOT), RALT(KC_SLASH),
+        EL_CIRC, EL_LABK, EL_RABK, EL_DLR,  EL_PERC,       EL_AT,   EL_AMPR, EL_ASTR, EL_QUOT, EL_GRV,
+        EL_LCBR, EL_LPRN, EL_RPRN, EL_RCBR, EL_EQL,  EL_BSLS, EL_PLUS, EL_MINS, EL_SLSH, EL_DQUO,
+        EL_TILD, EL_LBRC, EL_RBRC, EL_UNDS, EL_HASH,       EL_PIPE, EL_EXLM, EL_SCLN, EL_COLN, EL_QUES,
                                                  _______, _______,   _______,_______
     ),
 };
@@ -67,8 +68,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t *record, uint16_t prev_keycode) {
     switch (keycode) {
-        case HM_D:
-        case HM_K:
+        case HM_E:
+        case HM_N:
+        case HM_R:
+        case HM_T:
             return 0;
     }
 
@@ -90,7 +93,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         case HM_I:
         case HM_U:
             // Prevent accidental hold on same hand
-            return TAPPING_TERM + 1250;
+            return TAPPING_TERM + 300;
         default:
             return TAPPING_TERM;
     }
